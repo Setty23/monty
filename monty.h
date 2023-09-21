10110868ef20eaf3ad525b79db4c9f550c083621
Setty23 <125440661+Setty23@users.noreply.github.com>
2023-09-21 16:26:57 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -11,7 +11,7 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
+ * @nw: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
@@ -20,7 +20,7 @@
  */
 typedef struct stack_s
 {
-	int n;
+	int nw;
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
